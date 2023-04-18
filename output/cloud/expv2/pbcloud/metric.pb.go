1f83af2e2a523671137abe23aa0318e3be828fe0
Ivan <2103732+codebien@users.noreply.github.com>
2023-04-18 18:51:47 +0200

    Bump new proto version

--- output/cloud/expv2/pbcloud/metric.pb.go
+++ output/cloud/expv2/pbcloud/metric.pb.go
@@ -25,6 +25,8 @@ const (
 type MetricType int32
 
 const (
+	// Metric of type METRICTYPE_UNSPECIFIED declared, but not allowed.
+	// See: https://protobuf.dev/programming-guides/dos-donts/#do-include-an-unspecified-value-in-an-enum.
 	MetricType_METRICTYPE_UNSPECIFIED MetricType = 0
 	MetricType_COUNTER                MetricType = 1
 	MetricType_GAUGE                  MetricType = 2
@@ -84,6 +86,7 @@ type MetricSet struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Metrics []*Metric `protobuf:"bytes,1,rep,name=metrics,proto3" json:"metrics,omitempty"`
 }
 
@@ -134,6 +137,7 @@ type Metric struct {
 	// Required.
 	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
 	// Required.
+	// Can't be of type MetricType.METRICTYPE_UNSPECIFIED (which is zero value).
 	Type MetricType `protobuf:"varint,2,opt,name=type,proto3,enum=metrics.MetricType" json:"type,omitempty"`
 	// Optional.
 	TimeSeries []*TimeSeries `protobuf:"bytes,3,rep,name=time_series,json=timeSeries,proto3" json:"time_series,omitempty"`
@@ -256,9 +260,16 @@ type TimeSeries struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
+	//
+	// At minimum two labels should be present:
+	// - name: "__name__", value: "metric name this time series belongs to/is contained within",
+	// - name: "test_run_id", value: "cloud test run ID this time series belongs to".
 	Labels []*Label `protobuf:"bytes,1,rep,name=labels,proto3" json:"labels,omitempty"`
 	// time bucket size in seconds
 	AggregationPeriod uint32 `protobuf:"varint,2,opt,name=aggregation_period,json=aggregationPeriod,proto3" json:"aggregation_period,omitempty"`
+	// Required.
+	//
 	// Types that are assignable to Samples:
 	//
 	//	*TimeSeries_CounterSamples
@@ -382,6 +393,7 @@ type CounterSamples struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Values []*CounterValue `protobuf:"bytes,1,rep,name=values,proto3" json:"values,omitempty"`
 }
 
@@ -429,6 +441,7 @@ type GaugeSamples struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Values []*GaugeValue `protobuf:"bytes,1,rep,name=values,proto3" json:"values,omitempty"`
 }
 
@@ -476,6 +489,7 @@ type RateSamples struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Values []*RateValue `protobuf:"bytes,1,rep,name=values,proto3" json:"values,omitempty"`
 }
 
@@ -523,6 +537,7 @@ type TrendHdrSamples struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Values []*TrendHdrValue `protobuf:"bytes,1,rep,name=values,proto3" json:"values,omitempty"`
 }
 
@@ -570,6 +585,7 @@ type CounterValue struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Time  *timestamppb.Timestamp `protobuf:"bytes,1,opt,name=time,proto3" json:"time,omitempty"`
 	Value float64                `protobuf:"fixed64,2,opt,name=value,proto3" json:"value,omitempty"`
 }
@@ -625,6 +641,7 @@ type GaugeValue struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Time *timestamppb.Timestamp `protobuf:"bytes,1,opt,name=time,proto3" json:"time,omitempty"`
 	// last observed value
 	Last float64 `protobuf:"fixed64,2,opt,name=last,proto3" json:"last,omitempty"`
@@ -715,6 +732,7 @@ type RateValue struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Time         *timestamppb.Timestamp `protobuf:"bytes,1,opt,name=time,proto3" json:"time,omitempty"`
 	NonzeroCount uint32                 `protobuf:"varint,2,opt,name=nonzero_count,json=nonzeroCount,proto3" json:"nonzero_count,omitempty"`
 	TotalCount   uint32                 `protobuf:"varint,3,opt,name=total_count,json=totalCount,proto3" json:"total_count,omitempty"`
@@ -778,6 +796,7 @@ type TrendHdrValue struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
+	// Required.
 	Time *timestamppb.Timestamp `protobuf:"bytes,1,opt,name=time,proto3" json:"time,omitempty"`
 	// histogram parameter - value multiplier aka smallest value, default = 1.0
 	MinResolution float64 `protobuf:"fixed64,2,opt,name=min_resolution,json=minResolution,proto3" json:"min_resolution,omitempty"`
@@ -1030,10 +1049,10 @@ var file_metric_proto_rawDesc = []byte{
 	0x45, 0x44, 0x10, 0x00, 0x12, 0x0b, 0x0a, 0x07, 0x43, 0x4f, 0x55, 0x4e, 0x54, 0x45, 0x52, 0x10,
 	0x01, 0x12, 0x09, 0x0a, 0x05, 0x47, 0x41, 0x55, 0x47, 0x45, 0x10, 0x02, 0x12, 0x08, 0x0a, 0x04,
 	0x52, 0x41, 0x54, 0x45, 0x10, 0x03, 0x12, 0x09, 0x0a, 0x05, 0x54, 0x52, 0x45, 0x4e, 0x44, 0x10,
-	0x04, 0x42, 0x27, 0x5a, 0x25, 0x67, 0x6f, 0x2e, 0x6b, 0x36, 0x2e, 0x69, 0x6f, 0x2f, 0x6b, 0x36,
-	0x2f, 0x6f, 0x74, 0x70, 0x75, 0x74, 0x2f, 0x63, 0x6c, 0x6f, 0x75, 0x64, 0x2f, 0x65, 0x78, 0x70,
-	0x76, 0x32, 0x2f, 0x70, 0x62, 0x63, 0x6c, 0x6f, 0x75, 0x64, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x04, 0x42, 0x28, 0x5a, 0x26, 0x67, 0x6f, 0x2e, 0x6b, 0x36, 0x2e, 0x69, 0x6f, 0x2f, 0x6b, 0x36,
+	0x2f, 0x6f, 0x75, 0x74, 0x70, 0x75, 0x74, 0x2f, 0x63, 0x6c, 0x6f, 0x75, 0x64, 0x2f, 0x65, 0x78,
+	0x70, 0x76, 0x32, 0x2f, 0x70, 0x62, 0x63, 0x6c, 0x6f, 0x75, 0x64, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
